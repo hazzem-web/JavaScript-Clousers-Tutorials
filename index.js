a29c805e6bc4ea474fd296d497da20f56070de59
hazzem-web <hazzem.eljoker@gmail.com>
2026-03-19 02:47:51 +0200

    implement functions

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+/*
+function outer(){
+    let count = 0;
+    function inner(){
+        count++;
+        console.log(count);
+    }
+    return inner;
+}
+
+let counter = outer();
+
+counter() // 1
+counter() // 2
+counter() // 3
+*/
+
+
+
+
 function createCounter(){
     let count = 0;
     return { 
